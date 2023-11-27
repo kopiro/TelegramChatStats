43e9c573d21b7b808777cfb48bc88e7c640333b6
Flavio De Stefano <destefano.flavio@gmail.com>
2023-11-27 14:36:57 +0100

    Basic WA fixes

--- convert-whatsapp.py
+++ convert-whatsapp.py
@@ -1,20 +1,6 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
 # _*_ coding: utf-8 _*_
-
-"""
-@file 		print-results.py
-@author 	Simon Burkhardt - github.com/mnemocron
-@date 		2018.10.10
-
-This file prints the resulting numbers to the command line
-
-Post about this code:
-https://www.reddit.com/r/LongDistance/comments/9mgcol/oc_chat_statistics_from_telegram_using_python/
-
-Inspiration:
-https://www.reddit.com/r/LongDistance/comments/9jud8j/analysis_of_texts_from_a_long_distance/
-"""
 
 from __future__ import print_function
 
@@ -47,25 +33,9 @@ def load_file_to_srting(filename):
 
 
 def split_string_to_messages(string):
-    global dateseprarator
-    regex_matcher = "^\\d{2}.\\d{2}.\\d{2}, \\d{2}:\\d{2} - "
-    if "/" in string[:8]:
-        dateformat = "US"
-        dateseprarator = "/"
-        regex_matcher = "^\\d{2}/\\d{2}/\\d{4}, \\d{2}:\\d{2} - "
-        print(
-            "detected mm.dd.yy dateformat (using dateseprarator: "
-            + dateseprarator
-            + ")"
-        )
-    else:
-        print("using dd.mm.yy dateformat")
     messages = []
-    for line in string.split("\n"):
-        if re.match(regex_matcher, line):
-            messages.append(line)
-        else:
-            messages[-1] += "\n" + line
+    for line in string.split("\n["):
+        messages.append(line)
     return messages
 
 
@@ -75,34 +45,27 @@ def to_telegram_format(messages):
     data["chats"]["about"] = "This page lists all chats from this export."
     data["chats"]["list"] = []
     data["chats"]["list"].append({})
-    data["chats"]["list"][0]["name"] = (
-        messages[0].split("- ")[1].split(":")[0]
-    )  # name of the first message
+    data["chats"]["list"][0]["name"] = "unknown"
+    data["chats"]["list"][0]["id"] = 1
     data["chats"]["list"][0]["type"] = "personal_chat"
     data["chats"]["list"][0]["messages"] = []
 
     id = 0
     for message in messages:
         media = False
-        try:
-            date_time = message.split("-")[0].replace(" ", "")
-            date_ = date_time.split(",")[0]
-            if dateseprarator == ".":
-                date_y = 2000 + int(date_.split(".")[-1])
-                date_m = int(date_.split(".")[1])
-                date_d = int(date_.split(".")[0])
-            else:
-                date_y = int(date_.split("/")[-1])
-                date_m = int(date_.split("/")[1])
-                date_d = int(date_.split("/")[0])
-            time_ = date_time.split(",")[1]
-            time_h = time_.split(":")[0]
-            time_m = time_.split(":")[1]
-            name = message.split("- ")[1].split(":")[0]
-            text = message.split("- ", 1)[1].split(": ", 1)[1]
-        except Exception as e:
-            name = "automatic"
-            text = ""
+        msg_split = message.split("]", 1)
+        date_time = msg_split[0].replace('[', '')
+        date_ = date_time.split(",")[0]
+        date_y = int(date_.split("-")[-1])
+        date_m = int(date_.split("-")[1])
+        date_d = int(date_.split("-")[0])
+        time_ = date_time.split(",")[1]
+        time_h = time_.split(":")[0]
+        time_m = time_.split(":")[1]
+        time_s = time_.split(":")[2]
+        msg_1_split = msg_split[1].split(":", 1)
+        name = msg_1_split[0].strip()
+        text = msg_1_split[1].strip()
         if "<medi" in text.lower():
             media = True
         data["chats"]["list"][0]["messages"].append({})
@@ -118,14 +81,12 @@ def to_telegram_format(messages):
             + str(time_h).zfill(2)
             + ":"
             + str(time_m).zfill(2)
-            + ":00"
-        )  # 2017-06-12T21:30:09
+            + ":"
+            + str(time_s).zfill(2)
+        )
         data["chats"]["list"][0]["messages"][-1]["edited"] = "1970-01-01T01:00:00"
         data["chats"]["list"][0]["messages"][-1]["from"] = name
         data["chats"]["list"][0]["messages"][-1]["text"] = text
-        data["chats"]["list"][0]["messages"][-1][
-            "photo"
-        ] = "placeholder/path/to/photo.jpg"
         id += 1
     return data
 
@@ -137,8 +98,8 @@ def main():
         exit(0)
     raw = load_file_to_srting(opts.indir)
     messages = split_string_to_messages(raw)
-    formated = to_telegram_format(messages)
-    dump_to_json_file("__import__/whatsapp-result.json", formated)
+    formatted = to_telegram_format(messages)
+    dump_to_json_file("__import__/whatsapp-result.json", formatted)
 
 
 if __name__ == "__main__":
